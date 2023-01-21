670d0705c528b5b2d1884657f5f33cbba402a8f5
Alex Rafael <texas@alexs-air-2.lan>
2023-01-20 19:15:05 -0800

    Added error handling

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,9 @@ const isProduction = environment === 'production'
 //init app
 const app = express()
 
+//init ValidationError
+const { ValidationError } = require('sequelize');
+
 //------------------------ Middlewares ------------------------//
 //Morgan
 app.use(morgan('dev')) 
@@ -54,9 +57,37 @@ app.use(routes)
 
 
 
+//------------------------ Error Handling ------------------------//
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
 
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
 
-
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 
 module.exports = app
