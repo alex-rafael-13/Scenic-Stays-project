f05d2a73e35e85b8d95900e29ecfbcb464fa4386
Alex Rafael <texas@alexs-air-2.lan>
2023-01-20 21:02:50 -0800

    Added User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,19 @@
 //Init router
 const router = require('express').Router();
 
+//Import files
+const { restoreUser } = require('../../utils/auth.js');
+
+const { User } = require('../../db/models');
+
+//Middleware
+router.use(restoreUser)
+
 //Test if router is working
 router.post('/test', (req, res) => {
     res.json({reqBody: req.body})
 })
+
 
 
 module.exports = router;
