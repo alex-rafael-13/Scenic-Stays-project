db6cdb4d7cee952c66f94c6aa9b0199173ef569e
Alex Rafael <texas@alexs-air-2.lan>
2023-01-24 15:47:07 -0800

    Created migration and table for bookings

---
+++ backend/db/migrations/20230124232728-create-booking.js
@@ -0,0 +1,59 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {}
+
+if(process.env.NODE_ENV === 'production'){
+  options.schema = process.env.SCHEMA
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      spotId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Spots',
+          key: 'id'
+        }
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      startDate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+      },
+      endDate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Bookings'
+    await queryInterface.dropTable(options);
+  }
+};
