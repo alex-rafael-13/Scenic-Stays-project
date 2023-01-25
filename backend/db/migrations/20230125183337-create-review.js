d3d176e50f0d0f2a80fc354576289e470a1dc471
Alex Rafael <texas@alexs-air-2.lan>
2023-01-25 12:36:31 -0800

    fixed review migration file

--- backend/db/migrations/20230125183337-create-review.js
+++ backend/db/migrations/20230125183337-create-review.js
@@ -44,12 +44,12 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMSTAMP')
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMSTAMP')
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
     }, options);
   },
