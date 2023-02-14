19dbcf0f3ba8520ed11e9f26be570e4bf4dc3af5
alex-rafael-13 <rafael.alex1398@gmail.com>
2023-02-14 04:13:59 -0800

    Login page completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,16 @@
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFromPage";
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route exact path='/'>
+        <h1>Hello from Home Page</h1>
+      </Route>
+      <Route path='/login'>
+        < LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
 
