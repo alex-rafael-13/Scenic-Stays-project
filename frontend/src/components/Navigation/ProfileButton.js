146df97f57e7a863a7b13962ea90487623949b75
alex-rafael-13 <rafael.alex1398@gmail.com>
2023-02-14 06:27:37 -0800

    dropdown menu done

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,7 +1,54 @@
-export default function HomeButton(){
+import React, { useState, useEffect, useRef } from "react";
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
+
+
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef()
+  
+  useEffect(() => {
+    if(!showMenu) return
+
+    const closeMenu = (e) => {
+      if (!ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+    
+    document.addEventListener('click', closeMenu);
+    
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+  
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
   return (
-    <div style={{ color: "red", fontSize: "100px" }}>
-      <i className="fa-solid fa-user"></i>
-    </div>
+    <>
+      <button onClick={openMenu}>
+        <i className="fa-solid fa-user" />
+      </button>
+      <ul className={ulClassName} ref={ulRef}>
+        <li>{user.username}</li>
+        <li>{user.firstName} {user.lastName}</li>
+        <li>{user.email}</li>
+        <li>
+          <button onClick={logout}>Log Out</button>
+        </li>
+      </ul>
+    </>
   );
-};
+}
+
+export default ProfileButton;
