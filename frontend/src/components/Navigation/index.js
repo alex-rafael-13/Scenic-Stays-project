7cf3d0ed75e8ae99470e81146a82d25faf6cf2a9
alex-rafael-13 <rafael.alex1398@gmail.com>
2023-02-15 02:21:17 -0800

    css for nav bar done

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import logo from'./logo-image/logo-image.png'
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
-
+  const ulRef = useRef()
+  
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Home</NavLink>
-      </li>
-      {isLoaded && (
-        <li>
-          <ProfileButton user={sessionUser} />
-        </li>
-      )}
-    </ul>
+    <div className='header'>
+      <div className='header-contents'>
+        <div className='logo-container'>
+          <NavLink exact to="/">
+            <img src={logo}/>
+          </NavLink>
+        </div>
+        {isLoaded && (
+          <div className='profile-container'>
+            <div className='message' ref={ulRef}>
+            {sessionUser ?
+              (<div>Welcome {sessionUser.firstName}!</div>) : (<div>Sign In or Sign Up!</div>)}
+            </div> 
+            <div>
+              <ProfileButton user={sessionUser} />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
 
