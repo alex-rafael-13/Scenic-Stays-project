1ff82fb308e55873ad681cf54138aef6bb0caaa1
alex-rafael-13 <rafael.alex1398@gmail.com>
2023-03-20 07:23:38 -0700

    added spots list 404 page

--- frontend/src/components/SpotList/index.js
+++ frontend/src/components/SpotList/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as spotActions from '../../store/spots'
 import SpotCard from "../SpotCard/SpotCard";
@@ -10,6 +10,7 @@ import SingleSpot from "../SingleSpot/index";
 export default function SpotList(){
     const dispatch = useDispatch()
     const spots = useSelector(state => state.spots.spots)
+    const [spotsErr, setSpotsErr] = useState({})
     
     // useEffect(() => {
     //     dispatch(spotActions.resetSpot())
@@ -19,30 +20,31 @@ export default function SpotList(){
         dispatch(spotActions.retrieveAllSpots())
             .catch(
                 async res => {
-                    const data = await res.json
-                    if(data && !data.length){
-                        return (
-                            <h1>Unable to Retrieve Details. Please Try Again Later</h1>
-                        )
+                    const data = await res.json()
+                    if(data && data.statusCode >= 400 ){
+                        setSpotsErr(data)
                     }
                 }                              
             )
         dispatch(spotActions.resetSpot())
     }, [dispatch])
 
-    if(!spots || spots.length === 0){
-        return(
-            <h1></h1>
-        )
-    }
+    // if(!spots || spots.length === 0){
+    //     return(
+    //         <h1></h1>
+    //     )
+    // }
     
     return( 
         <>
+        {spotsErr.statusCode >= 400 && <h1>Unable to retrieve spots. Please try again shortly</h1>}
+        {spots &&
             <div className="spot-list">
                 {spots?.map(spot => (
                     <SpotCard key={spot.id} spot={spot}/>
-                ))}
+                    ))}
             </ div>
+        }
         </>
     )    
 }
