d043490330daf6e06c9dde3c66f5bd4170c29464
alex-rafael-13 <rafael.alex1398@gmail.com>
2023-08-03 13:39:52 -0700

    get user bookings thunk

--- frontend/src/components/Bookings/index.js
+++ frontend/src/components/Bookings/index.js
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { userBookings } from "../../store/bookings"
 
 
 export default function Bookings(){
+    const booking = useSelector(state => state.bookings.userBookings)
+    const dispatch = useDispatch()
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        dispatch(userBookings())
+            .then(() => setLoaded(true))
+    }, [dispatch])
+
     return(
     <>
-        Testing
+        {loaded &&
+            <div>testing</div>
+        }
     </>)
 }
